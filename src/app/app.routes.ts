7b6f049b5020686adf41ed395734d3e37ce04095
Tijana <tijana.markovic@yanfeng.com>
2026-02-23 13:17:56 +0100

    DODAT GRID

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login';
 import { Dashboard } from './dashboard/dashboard';
+import { ReportPage } from './report-page/report-page';
 
 export const routes: Routes = [
     {path:'login',component:LoginComponent},
     {path:'dashboard',component:Dashboard},
+    {path:'report-page',component:ReportPage},
     {path:'',redirectTo:'dashboard',pathMatch:'full'}
 
 ];
--- src/app/report-page/report-page.ts
+++ src/app/report-page/report-page.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { GridsterConfig, GridsterItem, GridsterModule }  from 'angular-gridster2';
+import { GridsterConfig, GridsterItem, GridsterModule } from 'angular-gridster2';
 
 @Component({
-  selector: 'app-dashboard',
+  selector: 'app-report-page',
   standalone: true,
   imports: [CommonModule, GridsterModule],
-  templateUrl: './dashboard.html',
-  styleUrl: './dashboard.css',
+  templateUrl: './report-page.html',
+  styleUrl: './report-page.css',
 })
-export class Dashboard implements OnInit {
+export class ReportPage implements OnInit {
   options!: GridsterConfig;
   dashboard!: Array<GridsterItem>;
 
@@ -18,10 +18,15 @@ export class Dashboard implements OnInit {
       draggable: {
         enabled: true,
       },
+      resizable: {
+        enabled: true,
+      },
       swap: false,
       pushItems: true,
       maxCols: 4,
-      compactType: 'compactUp'
+      minCols: 1,
+      defaultItemCols: 2,
+      defaultItemRows: 1,
     };
 
     this.dashboard = [
@@ -30,12 +35,7 @@ export class Dashboard implements OnInit {
       {cols: 1, rows: 1, y: 0, x: 4},
       {cols: 1, rows: 1, y: 2, x: 5},
       {cols: 1, rows: 1, y: 1, x: 0},
-      {cols: 1, rows: 1, y: 1, x: 0},
       {cols: 2, rows: 2, y: 3, x: 5},
-      {cols: 2, rows: 2, y: 2, x: 0},
-      {cols: 2, rows: 1, y: 2, x: 2},
-      {cols: 1, rows: 1, y: 2, x: 4},
-      {cols: 1, rows: 1, y: 2, x: 6}
     ];
   }
 
